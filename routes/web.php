f54414b4358a5296d7433729233e3aad71af766d
Nguyen Tan Phat <103521094+phatvippro2608@users.noreply.github.com>
2024-07-18 21:11:15 +0700

    Revert "themes: login page"
    This reverts commit 3d4065910dfb8eb5e376f89da100e1a59e919049.

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/login', function () {
-    return view('login');
-});
-
-Route::get('/home', function () {
-    return view('home');
+Route::get('/', function () {
+    return view('welcome');
 });
