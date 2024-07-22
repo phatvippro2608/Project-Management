4bc3d1e1e6cbd306ca1d97ce79cd03c88d3fdf02
Nguyen Tan Phat <103521094+phatvippro2608@users.noreply.github.com>
2024-07-22 15:37:14 +0700

    .

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -2,17 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AccountModel;
+use App\Models\EmployeeModel;
 use Illuminate\Http\Request;
 
 class AccountController extends Controller
 {
     function getView()
     {
-        return view('auth.account.account');
+        $account = AccountModel::getAll();
+        $employees = EmployeeModel::all();
+        return view('auth.account.account', ['account' => $account, 'employees' => $employees]);
     }
 
     function add(Request $request)
     {
-        
+
     }
 }
