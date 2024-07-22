77a1f19d7140ce82aa43eb24c4cb044d4e7c8db0
ttnhan2121 <ttnhan1212@hotmail.com>
2024-07-22 10:47:09 +0700

    .

--- resources/views/auth/employees/employees.blade.php
+++ resources/views/auth/employees/employees.blade.php
@@ -20,14 +20,14 @@
                     </div>
                     <div class="card-body m-1">
                         <div class="btn btn-primary mx-2">
-                            <div class="d-flex align-items-center">
+                            <div class="d-flex align-items-center at1">
                                 <span class="material-symbols-outlined">
                                     add
                                 </span>
                                 Add
                             </div>
                         </div>
-                        <div class="btn btn-success mx-2">
+                        <div class="btn btn-success mx-2 at2">
                             <div class="d-flex align-items-center">
                                 <span class="material-symbols-outlined">
                                     upload_2
@@ -63,19 +63,26 @@
                         <div class="table-responsive">
                             <table class="table card-table table-vcenter text-nowrap datatable">
                                 <thead>
-                                <tr>
-                                    <th>Employee Code</th>
-                                    <th>Photo</th>
-                                    <th>Full Name</th>
-                                    <th>English Name</th>
-                                    <th>Gender</th>
-                                    <th>Phone</th>
-                                    <th></th>
-                                </tr>
-
+                                    <tr>
+                                        <th>Employee Code</th>
+                                        <th>Photo</th>
+                                        <th>Full Name</th>
+                                        <th>English Name</th>
+                                        <th>Gender</th>
+                                        <th>Phone</th>
+                                        <th></th>
+                                    </tr>
                                 </thead>
                                 <tbody>
-
+                                @foreach($data as $item)
+                                    <tr>
+                                        <td>{{$item->employee_code}}</td>
+                                        <td>{{$item->photo}}</td>
+                                        <td>{{$item->first_name + $item->last_name}}</td>
+                                        <td>{{$item->en_name}}</td>
+                                        <td>{{$item->gender}}</td>
+                                    </tr>
+                                @endforeach
                                 </tbody>
                             </table>
                         </div>
@@ -84,4 +91,57 @@
             </div>
         </div>
     </section>
+    <div class="modal fade md1">
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title text-bold"></h4>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-md-12">
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary btn-add">Upload</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade md2">
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title text-bold"></h4>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <label for="">Choose file (*.xlsx) or download
+                            </label>
+                            <a href="">Example</a>
+                            <input accept=".xlsx" name="file-excel" type="file" class="form-control">
+                            <br>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary btn-upload">Upload</button>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+@section('script')
+    <script>
+        $('.at1').click(function () {
+            $('.md1 .modal-title').text('Add Employee');
+            $('.md1').modal('show');
+        });
+        $('.at2').click(function () {
+            $('.md2 .modal-title').text('Add Employee');
+            $('.md2').modal('show');
+        });
+    </script>
 @endsection
