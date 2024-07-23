3b4c015f5b74009fba9efa49e45f46a3c15db01f
Nguyen Tan Phat <103521094+phatvippro2608@users.noreply.github.com>
2024-07-23 14:16:03 +0700

    Update account

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -11,12 +11,26 @@ use Illuminate\Support\Facades\Session;
 class AccountController extends Controller
 {
     private $status = [1=>'Active', 2 => 'Offine', 3 => 'Locked'];
-    function getView()
+    private $permission = [1=>'Super Admin', 2 => 'Admin', 0 => 'User'];
+    function getView(Request $request)
     {
-        $account = AccountModel::getAll();
+        $perPage = intval(env('ITEM_PER_PAGE'));
+        $keyword = $request->input('keyw', '');
+
+        $account = EmployeeModel::query()
+            ->join('account', 'account.id_employee', '=', 'employees.id_employee')
+            ->when($keyword, function ($query) use ($keyword) {
+                $query->where('last_name', 'like', "%{$keyword}%")
+                    ->orWhere('first_name', 'like', "%{$keyword}%")
+                    ->orWhere('username', 'like', "%{$keyword}%")
+                    ->orWhere('employee_code', 'like', "%{$keyword}%");
+            })
+            ->paginate($perPage);
+
+//        $account = AccountModel::getAll();
         $employees = EmployeeModel::all();
         $status = $this->status;
-        return view('auth.account.account', ['account' => $account, 'employees' => $employees, 'status' => $this->status]);
+        return view('auth.account.account', ['account' => $account, 'employees' => $employees, 'status' => $this->status, 'permission' => $this->permission]);
     }
 
     static function permission()
@@ -24,8 +38,93 @@ class AccountController extends Controller
         return Session::get(StaticString::PERMISSION);
     }
 
+    function randomUserPwd($max=5) {
+        $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+        $pass = array();
+        $alphaLength = strlen($alphabet) - 1;
+        for ($i = 0; $i < $max; $i++) {
+            $n = rand(0, $alphaLength);
+            $pass[] = $alphabet[$n];
+        }
+        return implode($pass);
+    }
+
+    static function toAttrJson($data, $list = []){
+        if (count($list)){
+            $tmp = array();
+            $data = (array)$data;
+            foreach ($list as $key){
+                $tmp[$key] = $data[$key];
+            }
+            return json_encode($tmp);
+        }
+        return json_encode($data);
+    }
+
+    public static function status($message, $code)
+    {
+        return json_encode((object)["status" => $code, "message" => $message]);
+    }
+
     function add(Request $request)
     {
+        $id_employee = $request->input('id_employee');
+        $username = $request->input('username');
+        $password = $request->input('password');
+        $status = $request->input('status');
+        $permission = $request->input('permission');
+        $auto_pwd = $request->input('auto_pwd');
 
+        if($auto_pwd == 'true')
+            $password = $this->randomUserPwd(20);
+        $hashPass = password_hash($password, PASSWORD_BCRYPT);
+        $i = [
+            'id_employee' => $id_employee,
+            'username' => $username,
+            'email' => null,
+            'password' => $hashPass,
+            'status' => $status,
+            'permission' => $permission,
+        ];
+        if(AccountModel::insert($i)){
+            return $this->status('Thêm thành công', 200);
+        };
+        return $this->status('Thêm thất bại', 500);
+    }
+
+    function update(Request $request)
+    {
+        $id_account = $request->input('id_account');
+        $id_employee = $request->input('id_employee');
+        $username = $request->input('username');
+        $password = $request->input('password');
+        $status = $request->input('status');
+        $permission = $request->input('permission');
+        $auto_pwd = $request->input('auto_pwd');
+
+        if($auto_pwd == 'true')
+            $password = $this->randomUserPwd(20);
+        $hashPass = password_hash($password, PASSWORD_BCRYPT);
+        $i = [
+            'id_employee' => $id_employee,
+            'username' => $username,
+            'email' => null,
+            'password' => $hashPass,
+            'status' => $status,
+            'permission' => $permission,
+        ];
+        if(AccountModel::where('id_account',$id_account)->update($i)){
+            return $this->status('Cập nhật thành công', 200);
+        };
+        return $this->status('Cập nhật thất bại', 500);
+    }
+
+    function delete(Request $request)
+    {
+        $id_account = $request->input('id_account');
+        if(AccountModel::where('id_account',$id_account)->delete()){
+            return $this->status('Xóa tài khoản thành công', 200);
+        };
+        return $this->status('Xóa thất bại', 500);
     }
 }
--- resources/views/auth/account/account.blade.php
+++ resources/views/auth/account/account.blade.php
@@ -1,4 +1,5 @@
 @extends('auth.main')
+
 @section('contents')
     <div class="pagetitle">
         <h1>Account</h1>
@@ -14,7 +15,7 @@
         <div class="row">
             <div class="col">
                 <div class="card">
-                    <div class="card-body m-1 mt-3">
+                    <div class="card-header">
                         <div class="d-inline-flex align-items-center">
                             <div class="btn btn-primary mx-2 btn-add">
                                 <div class="d-flex align-items-center">
@@ -25,13 +26,26 @@
                                 </div>
                             </div>
                             <div class="ms-auto text-secondary">
-                                <div class="search-container">
-                                    <input type="text" class="form-control form-control-md" aria-label="Search invoice" placeholder="Search ...">
+                                {{--                                <form action="">--}}
+                                {{--                                    <div class="input-group input-group">--}}
+                                {{--                                        <input name="keyw" type="text"--}}
+                                {{--                                               value="{{ request()->input('keyw') }}"--}}
+                                {{--                                               class="form-control"--}}
+                                {{--                                               placeholder="Search ...">--}}
+                                {{--                                        <span class="input-group-btn">--}}
+                                {{--                                    <button type="submit" class="btn btn-info btn-flat">Search</button>--}}
+                                {{--                                    </span>--}}
+                                {{--                                    </div>--}}
+                                {{--                                </form>--}}
+                                <div class="search-container w-100">
+                                    <input type="text" class="form-control form-control-md" aria-label="Search invoice"
+                                           placeholder="Search ...">
                                     <i class="bi bi-search search-button"></i>
                                 </div>
                             </div>
                         </div>
-
+                    </div>
+                    <div class="card-body m-1">
                         <div class="table-responsive mt-4">
                             <table class="table card-table table-vcenter text-nowrap datatable">
                                 <thead>
@@ -47,51 +61,57 @@
 
                                 </thead>
                                 <tbody class="account-list">
-                                    @foreach($account as $item)
-                                        <tr class="account-item">
-                                            <td class="text-right">
-                                                <div class="d-flex align-items-center justify-content-center">
-                                                    <div class="action-buttons ">
-                                                        <a class=" edit">
-                                                            <i class="bi bi-pencil-square ic-update ic-btn" data="{{json_encode($item)}}"></i>
-                                                        </a>
-                                                        <a class=" delete">
-                                                            <i class="bi bi-trash ic-delete ic-btn" aria-hidden="true" data="{{ $item->id_account }}"></i>
-                                                        </a>
-                                                        <a class=" key">
-                                                            <i class="bi bi-key ic-key ic-btn" data="{{ $item->id_account }}"></i>
-                                                        </a>
-                                                    </div>
-                                                    <img src="http://ventech.local/assets/img/profile-img.jpg" alt="" class="account-photo rounded-circle p-0 m-0">
+                                @foreach($account as $item)
+                                    <tr class="account-item">
+                                        <td class="text-right">
+                                            <div class="d-flex align-items-center justify-content-center">
+                                                <div class="action-buttons ">
+                                                    <a class=" edit">
+                                                        <i class="bi bi-pencil-square ic-update ic-btn"
+                                                           data="{{(\App\Http\Controllers\AccountController::toAttrJson($item))}}"></i>
+                                                    </a>
+                                                    <a class=" delete">
+                                                        <i class="bi bi-trash ic-delete ic-btn" aria-hidden="true"
+                                                           data="{{ $item->id_account }}"></i>
+                                                    </a>
+                                                    <a class=" key">
+                                                        <i class="bi bi-key ic-key ic-btn"
+                                                           data="{{ $item->id_account }}"></i>
+                                                    </a>
                                                 </div>
+                                                <img src="http://ventech.local/assets/img/profile-img.jpg" alt=""
+                                                     class="account-photo rounded-circle p-0 m-0">
+                                            </div>
 
-                                            </td>
-                                            <td class="text-center">
-                                                {{$item->employee_code}} - {{$item->first_name}} {{$item->last_name}}
-                                            </td>
-                                            <td class="text-center">
-                                                {{$item->email}}
-                                            </td>
-                                            <td class="text-center">
-                                                {{$item->username}}
-                                            </td>
-                                            <td class="text-center">
-                                                @if($status==2)
-                                                    <i class="bi bi-circle-fill account-status" style="color: darkgray;"></i>
-                                                @else
-                                                    <i class="bi bi-circle-fill account-status"></i>
-                                                @endif
+                                        </td>
+                                        <td class="text-center">
+                                            {{$item->employee_code}} - {{$item->first_name}} {{$item->last_name}}
+                                        </td>
+                                        <td class="text-center">
+                                            {{$item->email}}
+                                        </td>
+                                        <td class="text-center">
+                                            {{$item->username}}
+                                        </td>
+                                        <td class="text-center">
+                                            @if($status[$item->status] == 'Offine')
+                                                <i class="bi bi-circle-fill account-status offine"></i>
+                                            @elseif($status[$item->status] == 'Locked')
+                                                <i class="bi bi-circle-fill account-status" style="color:red;"></i>
+                                            @else
+                                                <i class="bi bi-circle-fill account-status"></i>
+                                            @endif
 
-                                                    {{$status[$item->status]}}
-                                            </td>
-                                            <td class="text-center">
-                                                Active 16 hours ago
-                                            </td>
-                                            <td class="text-center">
-                                                16 years ago
-                                            </td>
-                                        </tr>
-                                    @endforeach
+                                            {{$status[$item->status]}}
+                                        </td>
+                                        <td class="text-center">
+                                            Active 16 hours ago
+                                        </td>
+                                        <td class="text-center">
+                                            16 years ago
+                                        </td>
+                                    </tr>
+                                @endforeach
                                 </tbody>
                             </table>
                         </div>
@@ -112,10 +132,12 @@
                             <label for="">
                                 Employee
                             </label>
-                            <select  class="form-select" aria-label="Default select example">
+                            <select class="form-select name1" aria-label="Default">
                                 <option value="-1">No select</option>
-                                <option value="123456">123456 - Nguyễn Văn A</option>
-                                <option value="123452">123456 - Nguyễn Văn B</option>
+                                @foreach($employees as $employee)
+                                    <option value="{{$employee->id_employee}}">{{$employee->employee_code}}
+                                        - {{$employee->first_name}} {{$employee->last_name}}</option>
+                                @endforeach
                             </select>
 
                         </div>
@@ -125,45 +147,160 @@
                             <label for="">
                                 Username
                             </label>
-                            <input  type="text" class="form-control name2">
+                            <input type="text" class="form-control name2">
                         </div>
                         <div class="col-md-12" style="margin-top: 1rem">
-                            <label for="">Password</label>
+                            <label for="" class="passName">Password</label> |
                             <label for="auto_pwd">Auto</label>
                             <input class="auto_pwd" type="checkbox" name="auto_pwd" checked>
-                            <input class="form-control name5" type="text" placeholder="">
+                            <input class="form-control name3" type="text" placeholder="">
                         </div>
-                        <div class="col-md-12" style="margin-top: 1rem">
+                        <div class="col-md-6" style="margin-top: 1rem">
                             <label for="">
                                 Status
                             </label>
-                            <select  class="form-select" aria-label="Default select example">
-                                <option value="1">Active</option>
+                            <select class="form-select name4" aria-label="Default">
+                                @foreach($status as $key => $val)
+                                    <option value="{{$key}}">{{$val}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="col-md-6" style="margin-top: 1rem">
+                            <label for="">
+                                Permission
+                            </label>
+                            <select class="form-select name5" aria-label="Default">
+                                @foreach($permission as $key => $val)
+                                    <option value="{{$key}}">{{$val}}</option>
+                                @endforeach
                             </select>
                         </div>
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="submit" class="btn btn-primary btn-upload">Create</button>
+                    <button type="button" class="btn btn-primary btn-upload at2">Create</button>
                 </div>
             </div>
         </div>
     </div>
 @endsection
 @section('script')
-<script>
-    $('.btn-add').click(function () {
-        $('.md1 .modal-title').text('Add Account');
-        $('.md1').modal('show');
-    });
+    <script>
+        $('.name3').prop('disabled', $('.auto_pwd').is(':checked'));
+        $('.auto_pwd').change(function () {
+            $('.name3').prop('disabled', $('.auto_pwd').is(':checked'));
+        })
 
-    $('.ic-update').click(function () {
-        $('.md1 .modal-title').text('Update Account');
-        $('.md1').modal('show');
-    })
+        $('.btn-add').click(function () {
+            $('.md1 .modal-title').text('Add Account');
+            $('.md1 .passName').text('Password');
+            $('.name5').val(0);
+            $('.md1').modal('show');
 
-    $('.ic-delete').click(function () {
-        $xn = alert('Xác nhận xóa tài khoản');
-    })
-</script>
+            $('.at2').click(function () {
+                $.ajax({
+                    url: `{{action('App\Http\Controllers\AccountController@add')}}`,
+                    type: "PUT",
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
+                    data: {
+                        'id_employee': $('.name1').val(),
+                        'username': $('.name2').val(),
+                        'password': $('.name3').val(),
+                        'status': $('.name4').val(),
+                        'permission': $('.name5').val(),
+                        'auto_pwd': $('.auto_pwd').is(':checked')
+                    },
+                    success: function (result) {
+                        result = JSON.parse(result);
+                        if (result.status === 200) {
+                            // toastr.success(result.message, "Thao tác thành công");
+                            alert(result.message);
+                            setTimeout(function () {
+                                window.location.reload();
+                            }, 500);
+                        } else {
+                            // toastr.error(result.message, "Thao tác thất bại");
+                            alert(result.message);
+                        }
+                    }
+                });
+            });
+        });
+
+        $(document).on('click', '.ic-update', function () {
+            $('.md1 .modal-title').text('Update Account');
+            $('.md1 .passName').text('New Password');
+            var data = JSON.parse($(this).attr('data'));
+            $('.name1').val(data.id_employee);
+            $('.name2').val(data.username);
+            $('.name4').val(data.status);
+            $('.name5').val(data.permission);
+            $('.md1').modal('show');
+
+            $('.at2').click(function () {
+                $.ajax({
+                    url: `{{action('App\Http\Controllers\AccountController@update')}}`,
+                    type: "POST",
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
+                    data: {
+                        'id_account': data.id_account,
+                        'id_employee': $('.name1').val(),
+                        'username': $('.name2').val(),
+                        'password': $('.name3').val(),
+                        'status': $('.name4').val(),
+                        'permission': $('.name5').val(),
+                        'auto_pwd': $('.auto_pwd').is(':checked')
+                    },
+                    success: function (result) {
+                        result = JSON.parse(result);
+                        if (result.status === 200) {
+                            // toastr.success(result.message, "Thao tác thành công");
+                            alert(result.message);
+                            setTimeout(function () {
+                                window.location.reload();
+                            }, 500);
+                        } else {
+                            // toastr.error(result.message, "Thao tác thất bại");
+                            alert(result.message);
+                        }
+                    }
+                });
+            });
+        });
+
+
+        $('.ic-delete').click(function () {
+            if (!confirm("Chọn vào 'YES' để xác nhận xóa thông tin?\nSau khi xóa dữ liệu sẽ không thể phục hồi lại được.")) {
+                return;
+            }
+            var id = $(this).attr('data');
+            $.ajax({
+                url: `{{action('App\Http\Controllers\AccountController@delete')}}`,
+                type: "DELETE",
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+                data: {
+                    'id_account': id,
+                },
+                success: function (result) {
+                    result = JSON.parse(result);
+                    if (result.status === 200) {
+                        alert(result.message);
+                        setTimeout(function () {
+                            window.location.reload();
+                        }, 500);
+                    } else {
+                        alert(result.message);
+                    }
+                }
+            });
+        })
+
+    </script>
 @endsection
+
