07d7a9f2bfc5b9dbd4091167c12444f6235ff088
Nguyen Tan Phat <103521094+phatvippro2608@users.noreply.github.com>
2024-07-24 21:02:41 +0700

    .

--- resources/views/auth/dashboard/dashboard.blade.php
+++ resources/views/auth/dashboard/dashboard.blade.php
@@ -13,80 +13,91 @@
 
     <section class="section dashboard">
         <div class="row">
-
             <div class="col-lg-12">
                 <div class="row">
-                    <div class="col-xxl-2 col-md-6">
-                        <div class="card info-card sales-card ">
+                    <h4 class="fw-bold mb-3"><i class="bi bi-bar-chart-line"></i> Tổng quan</h4>
+                    <div class="col-xxl-2 col-xl-4 col-md-6">
+                        <div class="card info-card sales-card">
                             <div class="card-body">
-                                <h5 class="card-title"><b>Tổng thành viên</b></h5>
+                                <h5 class="card-title"><b>Employee</b></h5>
                                 <div class="d-flex align-items-center">
-                                    <div class="ps-3">
-                                        <h6>100</h6>
+                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                        <i class="bi bi-person"></i>
                                     </div>
-                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center ms-3">
-                                        <i class="bi bi-cart"></i>
+                                    <div class="ps-3">
+                                        <h6>{{$em_c}}</h6>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    <div class="col-xxl-2 col-md-6">
+                    <div class="col-xxl-2 col-xl-4 col-md-6">
                         <div class="card info-card sales-card">
                             <div class="card-body">
                                 <h5 class="card-title"><b>Team</b></h5>
                                 <div class="d-flex align-items-center">
+                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                        <i class="bi bi-people"></i>
+                                    </div>
                                     <div class="ps-3">
                                         <h6>100</h6>
                                     </div>
-                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center ms-3">
-                                        <i class="bi bi-cart"></i>
-                                    </div>
                                 </div>
                             </div>
-
                         </div>
                     </div>
 
-                    <div class="col-xxl-2 col-md-6">
+                    <div class="col-xxl-2 col-xl-4 col-md-6">
                         <div class="card info-card sales-card">
                             <div class="card-body">
-                                <h5 class="card-title"><b>Kế hoạch</b></h5>
+                                <h5 class="card-title"><b>Project</b></h5>
                                 <div class="d-flex align-items-center">
+                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                        <i class="bi bi-briefcase"></i>
+                                    </div>
                                     <div class="ps-3">
                                         <h6>100</h6>
                                     </div>
-                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center ms-3">
-                                        <i class="bi bi-cart"></i>
-                                    </div>
                                 </div>
                             </div>
-
                         </div>
                     </div>
 
-                    <div class="col-xxl-2 col-md-6">
+                    <div class="col-xxl-2 col-xl-4 col-md-6">
                         <div class="card info-card sales-card">
                             <div class="card-body">
-                                <h5 class="card-title"><b>Công việc</b></h5>
+                                <h5 class="card-title"><b>Task</b></h5>
                                 <div class="d-flex align-items-center">
+                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                        <i class="bi bi-list-task"></i>
+                                    </div>
                                     <div class="ps-3">
                                         <h6>100</h6>
                                     </div>
-                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center ms-3">
-                                        <i class="bi bi-cart"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-xxl-2 col-xl-4 col-md-6">
+                        <div class="card info-card sales-card">
+                            <div class="card-body">
+                                <h5 class="card-title"><b>Subtask</b></h5>
+                                <div class="d-flex align-items-center">
+                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                        <i class="bi bi-list-task"></i>
+                                    </div>
+                                    <div class="ps-3">
+                                        <h6>100</h6>
                                     </div>
                                 </div>
                             </div>
-
                         </div>
                     </div>
 
                 </div>
             </div>
-
-
         </div>
     </section>
 @endsection
