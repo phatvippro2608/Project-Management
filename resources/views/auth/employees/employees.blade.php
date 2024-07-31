2e964a98056872c8556522e8680c7713a3208888
ttnhan2121 <ttnhan1212@hotmail.com>
2024-07-31 07:35:44 +0700

    .

--- resources/views/auth/employees/employees.blade.php
+++ resources/views/auth/employees/employees.blade.php
@@ -42,10 +42,14 @@
                     </div>
                     <div class="col-md-6 m-0">
                         <div class="input-group ms-sm-auto w-50">
-                            <button class="input-group-text bg-transparent border-secondary rounded-start-4">
-                                <i class="bi bi-search"></i>
-                            </button>
-                            <input type="text" class="form-control border-start-0 border-secondary rounded-end-4">
+                            <form method="GET"
+                                  action="{{ action('App\Http\Controllers\EmployeesController@getView') }}"
+                                  class="d-flex w-100">
+                                <button type="submit" class="input-group-text bg-transparent border-secondary rounded-start-4">
+                                    <i class="bi bi-search"></i>
+                                </button>
+                                <input name="keyw" type="text" class="form-control border-start-0 border-secondary rounded-end-4" value="{{ request()->input('keyw') }}" placeholder="Search Employee">
+                            </form>
                         </div>
                     </div>
                 </div>
