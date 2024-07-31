3ee164b9cfabd7e27544e81db00ccd69aa075d6d
baothanngg <21022019@st.vlute.edu.vn>
2024-07-31 07:46:27 +0700

    .

--- resources/views/auth/leave/earn-leave.blade.php
+++ resources/views/auth/leave/earn-leave.blade.php
@@ -20,7 +20,6 @@
             background-color: #1472e5;
             clip-path: polygon(0 0, 100% 0, 0 100%);
         }
-
     </style>
     <div class="container mt-4">
         <div class="d-flex justify-content-between align-items-center bg-white p-3 mb-3">
@@ -39,11 +38,12 @@
         </div>
         <div class="d-flex justify-content-between align-items-center mb-3">
             <div>
-                <button class="btn btn-primary" id="addApplicationBtn" data-bs-toggle="modal" data-bs-target="#addApplicationModal">
+                <button class="btn btn-primary" id="addApplicationBtn" data-bs-toggle="modal"
+                    data-bs-target="#addApplicationModal">
                     <i class="bi bi-plus-circle"></i> Assign Earned Leave
                 </button>
-                {{--                <button class="btn btn-secondary" id="leaveApplicationBtn"><i class="bi bi-list"></i> Leave--}}
-                {{--                    Application</button>--}}
+                {{--                <button class="btn btn-secondary" id="leaveApplicationBtn"><i class="bi bi-list"></i> Leave --}}
+                {{--                    Application</button> --}}
             </div>
 
         </div>
@@ -55,21 +55,21 @@
             <div style="height: 100vh;">
                 <table id="applicationTable" class="table table-bordered mt-3 mb-3">
                     <thead>
-                    <tr>
+                        <tr>
 
-                        <th class="text-start">Employee Pin</th>
-                        <th>Employee Name</th>
-                        <th>Total Hour</th>
-                    </tr>
+                            <th class="text-start">Employee Pin</th>
+                            <th>Employee Name</th>
+                            <th>Total Hour</th>
+                        </tr>
                     </thead>
                     <tbody>
-                    @foreach ($earn_leave as $item)
-                        <tr>
-                            <td class="text-start">{{$item->employee_code}}</td>
-                            <td>{{$item->employee_name}}</td>
-                            <td>{{$item->totalhour}} hour</td>
-                        </tr>
-                    @endforeach
+                        @foreach ($earn_leave as $item)
+                            <tr>
+                                <td class="text-start">{{ $item->employee_code }}</td>
+                                <td>{{ $item->employee_name }}</td>
+                                <td>{{ $item->totalhour }} hour</td>
+                            </tr>
+                        @endforeach
 
                     </tbody>
                 </table>
@@ -80,7 +80,8 @@
     </div>
 
     <!-- Add Application Modal -->
-    <div class="modal fade" id="addApplicationModal" tabindex="-1" aria-labelledby="addApplicationModalLabel" aria-hidden="true">
+    <div class="modal fade" id="addApplicationModal" tabindex="-1" aria-labelledby="addApplicationModalLabel"
+        aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -92,27 +93,27 @@
                         @csrf
                         <div class="mb-3">
                             <label for="name" class="form-label">Employee Name</label>
-                            {{--                            <input type="text" class="form-control" id="employee_name" name="employee_name" required>--}}
+                            {{--                            <input type="text" class="form-control" id="employee_name" name="employee_name" required> --}}
                             <select class="form-select" aria-label="Default" name="employee_id">
                                 <option value="">No select</option>
-                                @foreach($employees as $item)
-                                    <option value="{{$item->id_employee}}">{{$item->employee_code}}
-                                        - {{$item->first_name}} {{$item->last_name}}</option>
+                                @foreach ($employees as $item)
+                                    <option value="{{ $item->id_employee }}">{{ $item->employee_code }}
+                                        - {{ $item->first_name }} {{ $item->last_name }}</option>
                                 @endforeach
                             </select>
                         </div>
                         <div class="mb-3">
                             <label for="start_date" class="form-label">PIN</label>
-                            <input type="text" class="form-control" id="pin" name="pin" required>
+                            <input type="text" class="form-control" id="pin" name="pin" readonly>
                         </div>
                         <div class="mb-3">
                             <label for="end_date" class="form-label">Leave Type</label>
-                            {{--                            <input type="text" class="form-control" id="leave_type" name="leave_type" required>--}}
+                            {{--                            <input type="text" class="form-control" id="leave_type" name="leave_type" required> --}}
                             <select class="form-select" aria-label="Default" name="leave_type">
                                 <option value="">No select</option>
-{{--                                @foreach($leave_type as $item)--}}
-{{--                                    <option value="{{$item->id}}">{{$item->leave_type}}</option>--}}
-{{--                                @endforeach--}}
+                                {{--                                @foreach ($leave_type as $item) --}}
+                                {{--                                    <option value="{{$item->id}}">{{$item->leave_type}}</option> --}}
+                                {{--                                @endforeach --}}
                             </select>
                         </div>
                         <div class="mb-3">
@@ -144,7 +145,8 @@
 
 
 
-    <div class="modal fade" id="editApplicationModal" tabindex="-1" aria-labelledby="editApplicationModal" aria-hidden="true">
+    <div class="modal fade" id="editApplicationModal" tabindex="-1" aria-labelledby="editApplicationModal"
+        aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -157,27 +159,27 @@
                         @method('PUT')
                         <div class="mb-3">
                             <label for="name" class="form-label">Employee Name</label>
-                            {{--                            <input type="text" class="form-control" id="employee_name" name="employee_name" required>--}}
+                            {{--                            <input type="text" class="form-control" id="employee_name" name="employee_name" required> --}}
                             <select class="form-select" aria-label="Default" name="employee_id" id="edit_employee_id">
                                 <option value="">No select</option>
-{{--                                @foreach($employee_name as $item)--}}
-{{--                                    <option value="{{$item->id_employee}}">{{$item->employee_code}}--}}
-{{--                                        - {{$item->first_name}} {{$item->last_name}}</option>--}}
-{{--                                @endforeach--}}
+                                {{--                                @foreach ($employee_name as $item) --}}
+                                {{--                                    <option value="{{$item->id_employee}}">{{$item->employee_code}} --}}
+                                {{--                                        - {{$item->first_name}} {{$item->last_name}}</option> --}}
+                                {{--                                @endforeach --}}
                             </select>
                         </div>
                         <div class="mb-3">
                             <label for="start_date" class="form-label">PIN</label>
-                            <input type="text" class="form-control" id="edit_pin" name="pin" required>
+                            <input type="text" class="form-control" id="edit_pin" name="pin" readonly>
                         </div>
                         <div class="mb-3">
                             <label for="end_date" class="form-label">Leave Type</label>
-                            {{--                            <input type="text" class="form-control" id="leave_type" name="leave_type" required>--}}
+                            {{--                            <input type="text" class="form-control" id="leave_type" name="leave_type" required> --}}
                             <select class="form-select" aria-label="Default" name="leave_type" id="edit_leave_type">
                                 <option value="">No select</option>
-{{--                                @foreach($leave_type as $item)--}}
-{{--                                    <option value="{{$item->id}}">{{$item->leave_type}}</option>--}}
-{{--                                @endforeach--}}
+                                {{--                                @foreach ($leave_type as $item) --}}
+                                {{--                                    <option value="{{$item->id}}">{{$item->leave_type}}</option> --}}
+                                {{--                                @endforeach --}}
                             </select>
                         </div>
                         <div class="mb-3">
@@ -198,7 +200,8 @@
                         </div>
                         <div class="mb-3">
                             <label for="days" class="form-label">Leaves Status</label>
-                            <input type="number" class="form-control" id="edit_leave_status" name="leave_status" readonly>
+                            <input type="number" class="form-control" id="edit_leave_status" name="leave_status"
+                                readonly>
                         </div>
                         <button type="submit" class="btn btn-primary">Edit Application</button>
                     </form>
@@ -222,9 +225,9 @@
 
 @section('head')
     <link rel="stylesheet" type="text/css"
-          href="https://cdn.datatables.net/buttons/2.1.1/css/buttons.dataTables.min.css">
+        href="https://cdn.datatables.net/buttons/2.1.1/css/buttons.dataTables.min.css">
     <script type="text/javascript" charset="utf8"
-            src="https://cdn.datatables.net/buttons/2.1.1/js/dataTables.buttons.min.js"></script>
+        src="https://cdn.datatables.net/buttons/2.1.1/js/dataTables.buttons.min.js"></script>
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/2.1.1/js/buttons.flash.min.js">
     </script>
     <script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js">
@@ -236,4 +239,3 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 @endsection
-
