9bd25889871c7b7ef0f24303f84cf36702f95e9e
HuuThoCSE <huutho.cse@gmail.com>
2024-08-01 16:44:37 +0700

    Update Recognitions

--- app/Models/RecognitionModel.php
+++ app/Models/RecognitionModel.php
@@ -11,25 +11,39 @@ class RecognitionModel extends Model
     use HasFactory;
     protected $table = 'recognitions';
 
+    protected $fillable = [
+        'employee_id',
+        'recognition_type_id',
+        'recognition_date',
+        'description'
+    ];
+
     public function getRecognitions()
     {
-        $perPage = intval(env('ITEM_PER_PAGE', 15)); // Mặc định là 15 nếu không tìm thấy giá trị trong file .env
-        return DB::table('employees')
-            ->join('recognitions', 'employees.employee_id', '=', 'recognitions.employee_id')
-            ->join('recognition_types', 'recognition_types.recognition_type_id', '=', 'recognitions.recognition_type_id')
-            ->select('employees.*', 'recognitions.*', 'recognition_types.*') // Chọn các cột cần thiết
-            ->paginate($perPage);
+        try {
+            // Lấy dữ liệu với left join các bảng liên quan và phân trang
+            $recognitions = DB::table('recognitions')
+                ->Join('employees', 'employees.employee_id', '=', 'recognitions.employee_id')
+                ->Join('recognition_types', 'recognition_types.recognition_type_id', '=', 'recognitions.recognition_type_id')
+                ->select(
+                    'employees.employee_id', 'employees.employee_code', 'employees.last_name', 'employees.first_name', // Chọn các cột cần thiết từ bảng employees
+                    'recognitions.recognition_id', 'recognitions.recognition_date', // Chọn các cột cần thiết từ bảng recognitions
+                    'recognition_types.recognition_type_id', 'recognition_types.recognition_type_name' // Chọn các cột cần thiết từ bảng recognition_types
+                )->get();
+            return $recognitions;
+        } catch (\Exception $e) {
+            // Xử lý lỗi nếu có
+            return response()->json([
+                'status' => 'error',
+                'message' => $e->getMessage()
+            ], 500);
+        }
     }
+
 
     public static function getRecognitionTypes()
     {
         return DB::table('recognition_types')->get();
     }
 
-    protected $fillable = [
-        'employee_id',
-        'recognition_type_id',
-        'description',
-        'recognition_date'
-    ];
 }
