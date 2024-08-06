0de9a13e6cc164efbdcaac9b14ff1f5b0cca1a23
HuuThoCSE <huutho.cse@gmail.com>
2024-08-06 09:34:02 +0700

    Update

--- app/Models/RecognitionTypeModel.php
+++ app/Models/RecognitionTypeModel.php
@@ -4,13 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class RecognitionTypeModel extends Model
 {
     use HasFactory;
+
     protected $table = 'recognition_types';
 
     protected $fillable = [
-        'recognition_type_name'
+        'recognition_type_name',
+        'recognition_type_hidden',
     ];
+
+    public static function getRecognitionTypes()
+    {
+        return self::all();
+    }
 }
