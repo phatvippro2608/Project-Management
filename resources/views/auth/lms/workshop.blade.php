73b8e09277153b53f7f0f55a678537a14f7f9824
HuuThoCSE <huutho.cse@gmail.com>
2024-08-07 16:52:45 +0700

    Update lms workshop

--- resources/views/auth/lms/workshop.blade.php
+++ resources/views/auth/lms/workshop.blade.php
@@ -4,5 +4,15 @@
 @endsection
 
 @section('contents')
-    <h1>Hello</h1>
+<div class="pagetitle">
+    <h1>Workshops</h1>
+    <nav>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="/">Home</a></li>
+            <li class="breadcrumb-item"><a href="{{ action('App\Http\Controllers\LMSDashboardController@getView') }}">LMS</a></li>
+            <li class="breadcrumb-item active" href="{{ action('App\Http\Controllers\WorkshopController@getViewDashboard') }}">Workshop</li>
+        </ol>
+    </nav>
+</div>
+
 @endsection
