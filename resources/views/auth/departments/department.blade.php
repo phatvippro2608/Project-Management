1b2695deea551fe044b827871e0effa3010997a2
baothanngg <21022019@st.vlute.edu.vn>
2024-08-12 14:06:28 +0700

    .

--- resources/views/auth/departments/department.blade.php
+++ resources/views/auth/departments/department.blade.php
@@ -19,7 +19,7 @@
             <div class="modal-content">
 
                 <div class="modal-header">
-                    <h4 class="modal-title">Add Department</h4>
+                    <h4 class="modal-title">{{ __('messages.add_department') }}</h4>
                     @component('auth.component.btnCloseModal')@endcomponent
                 </div>
 
@@ -28,8 +28,7 @@
                         @csrf
                         <div class="mb-3">
                             <label for="department_name" class="form-label">Department Name</label>
-                            <input type="text" class="form-control" id="department_name" name="department_name"
-                                   required>
+                            <input type="text" class="form-control" id="department_name" name="department_name" required>
                         </div>
                         <button type="submit" class="btn btn-primary">Add Department</button>
                     </form>
@@ -39,12 +38,13 @@
     </div>
 
     <div class="modal fade" id="editDepartmentModal" tabindex="-1" aria-labelledby="editDepartmentModalLabel"
-         aria-hidden="true">
+        aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="editDepartmentModalLabel">Edit Department</h5>
-                    @component('auth.component.btnCloseModal')@endcomponent
+                    @component('auth.component.btnCloseModal')
+                    @endcomponent
                 </div>
                 <div class="modal-body">
                     <form id="editDepartmentForm">
@@ -54,7 +54,7 @@
                         <div class="mb-3">
                             <label for="edit_department_name" class="form-label">Department Name</label>
                             <input type="text" class="form-control" id="edit_department_name" name="department_name"
-                                   required>
+                                required>
                         </div>
                         <button type="submit" class="btn btn-primary">Save changes</button>
                     </form>
@@ -81,35 +81,33 @@
     </div>
 
     <div class="card shadow-sm p-3 mb-5 bg-white rounded-4">
-        <h3 class="text-left mb-4">Departments List</h3>
+        {{--        <h3 class="text-left mb-4">Departments List</h3> --}}
         <table id="departmentsTable" class="table table-hover table-borderless">
             <thead class="table-light">
-            <tr>
-                <th>ID</th>
-                <th>Department Name</th>
-                <th>Action</th>
-            </tr>
+                <tr>
+                    <th>ID</th>
+                    <th>Department Name</th>
+                    <th>Action</th>
+                </tr>
             </thead>
             <tbody id="departmentsTableBody">
-            @foreach ($departments as $department)
-                <tr>
-                    <td>{{ $department->department_id }}</td>
-                    <td>{{ $department->department_name }}</td>
-                    <td>
-                        <button
-                            class="btn p-0 btn-primary border-0 bg-transparent text-primary shadow-none edit-btn"
-                            data-id="{{ $department->department_id }}">
-                            <i class="bi bi-pencil-square"></i>
-                        </button>
-                        |
-                        <button
-                            class="btn p-0 btn-primary border-0 bg-transparent text-danger shadow-none delete-btn"
-                            data-id="{{ $department->department_id }}">
-                            <i class="bi bi-trash3"></i>
-                        </button>
-                    </td>
-                </tr>
-            @endforeach
+                @foreach ($departments as $department)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $department->department_name }}</td>
+                        <td>
+                            <button class="btn p-0 btn-primary border-0 bg-transparent text-primary shadow-none edit-btn"
+                                data-id="{{ $department->department_id }}">
+                                <i class="bi bi-pencil-square"></i>
+                            </button>
+                            |
+                            <button class="btn p-0 btn-primary border-0 bg-transparent text-danger shadow-none delete-btn"
+                                data-id="{{ $department->department_id }}">
+                                <i class="bi bi-trash3"></i>
+                            </button>
+                        </td>
+                    </tr>
+                @endforeach
             </tbody>
         </table>
     </div>
@@ -117,10 +115,12 @@
 
 @section('script')
     <script>
-        $(document).ready(function () {
+        $(document).ready(function() {
             var table = $('#departmentsTable').DataTable({
-                language: { search: "" },
-                initComplete: function (settings, json) {
+                language: {
+                    search: ""
+                },
+                initComplete: function(settings, json) {
                     $('.dt-search').addClass('input-group');
                     $('.dt-search').prepend(`<button class="input-group-text bg-secondary-subtle border-secondary-subtle rounded-start-4">
                                 <i class="bi bi-search"></i>
@@ -129,41 +129,43 @@
                 responsive: true
             });
 
-            $('#addDepartmentForm').submit(function (e) {
+            $('#addDepartmentForm').submit(function(e) {
                 e.preventDefault();
 
                 $.ajax({
                     url: '{{ route('departments.store') }}',
                     method: 'POST',
                     data: $(this).serialize(),
-                    success: function (response) {
+                    success: function(response) {
                         if (response.success) {
                             $('#addDepartmentModal').modal('hide');
                             // $('#successModal').modal('show');
                             toastr.success(response.message, "Successful");
 
-                            table.row.add([
-                                response.department.department_name,
-                                '<button class="btn p-0 btn-primary border-0 bg-transparent text-primary shadow-none edit-btn" data-id="' +
-                                response.department.department_id + '">' +
-                                '<i class="bi bi-pencil-square"></i>' +
-                                '</button>' +
-                                ' | ' +
-                                '<button class="btn p-0 btn-primary border-0 bg-transparent text-danger shadow-none delete-btn" data-id="' +
-                                response.department.department_id + '">' +
-                                '<i class="bi bi-trash3"></i>' +
-                                '</button>'
-                            ]).draw();
-                            $('#addDepartmentForm')[0].reset();
+                            // table.row.add([
+                            //     response.department.department_name,
+                            //     '<button class="btn p-0 btn-primary border-0 bg-transparent text-primary shadow-none edit-btn" data-id="' +
+                            //     response.department.department_id + '">' +
+                            //     '<i class="bi bi-pencil-square"></i>' +
+                            //     '</button>' +
+                            //     ' | ' +
+                            //     '<button class="btn p-0 btn-primary border-0 bg-transparent text-danger shadow-none delete-btn" data-id="' +
+                            //     response.department.department_id + '">' +
+                            //     '<i class="bi bi-trash3"></i>' +
+                            //     '</button>'
+                            // ]).draw();
+                            // $('#addDepartmentForm')[0].reset();
+                            location.reload();
                         }
                     },
-                    error: function (xhr) {
+                    error: function(xhr) {
                         toastr.error(response.message, "Error");
                     }
                 });
             });
 
-            $('#departmentsTable').on('click', '.edit-btn', function () {
+
+            $('#departmentsTable').on('click', '.edit-btn', function() {
                 var departmentId = $(this).data('id');
 
                 if (!departmentId) {
@@ -174,18 +176,18 @@
                 $.ajax({
                     url: '{{ url('departments') }}/' + departmentId + '/edit',
                     method: 'GET',
-                    success: function (response) {
+                    success: function(response) {
                         $('#editDepartmentId').val(response.department.department_id);
                         $('#edit_department_name').val(response.department.department_name);
                         $('#editDepartmentModal').modal('show');
                     },
-                    error: function (xhr) {
+                    error: function(xhr) {
                         alert('An error occurred.');
                     }
                 });
             });
 
-            $('#editDepartmentForm').submit(function (e) {
+            $('#editDepartmentForm').submit(function(e) {
                 e.preventDefault();
                 var departmentId = $('#editDepartmentId').val();
 
@@ -193,34 +195,35 @@
                     url: '{{ url('departments') }}/' + departmentId,
                     method: 'PUT',
                     data: $(this).serialize(),
-                    success: function (response) {
+                    success: function(response) {
                         if (response.success) {
                             $('#editDepartmentModal').modal('hide');
                             toastr.success("Edit successful");
 
-                            var row = table.row($('button[data-id="' + departmentId + '"]')
-                                .parents('tr'));
-                            row.data([
-                                response.department.department_name,
-                                '<button class="btn p-0 btn-primary border-0 bg-transparent text-primary shadow-none edit-btn" data-id="' +
-                                response.department.department_id + '">' +
-                                '<i class="bi bi-pencil-square"></i>' +
-                                '</button>' +
-                                ' | ' +
-                                '<button class="btn p-0 btn-primary border-0 bg-transparent text-danger shadow-none delete-btn" data-id="' +
-                                response.department.department_id + '">' +
-                                '<i class="bi bi-trash3"></i>' +
-                                '</button>'
-                            ]).draw();
+                            // var row = table.row($('button[data-id="' + departmentId + '"]')
+                            //     .parents('tr'));
+                            // row.data([
+                            //     response.department.department_name,
+                            //     '<button class="btn p-0 btn-primary border-0 bg-transparent text-primary shadow-none edit-btn" data-id="' +
+                            //     response.department.department_id + '">' +
+                            //     '<i class="bi bi-pencil-square"></i>' +
+                            //     '</button>' +
+                            //     ' | ' +
+                            //     '<button class="btn p-0 btn-primary border-0 bg-transparent text-danger shadow-none delete-btn" data-id="' +
+                            //     response.department.department_id + '">' +
+                            //     '<i class="bi bi-trash3"></i>' +
+                            //     '</button>'
+                            // ]).draw();
+                            location.reload();
                         }
                     },
-                    error: function (xhr) {
+                    error: function(xhr) {
                         toastr.error("Error");
                     }
                 });
             });
 
-            $('#departmentsTable').on('click', '.delete-btn', function () {
+            $('#departmentsTable').on('click', '.delete-btn', function() {
                 var departmentId = $(this).data('id');
                 var row = $(this).parents('tr');
 
@@ -240,12 +243,12 @@
                             data: {
                                 _token: '{{ csrf_token() }}'
                             },
-                            success: function (response) {
+                            success: function(response) {
                                 if (response.success) {
                                     table.row(row).remove().draw();
                                 }
                             },
-                            error: function (xhr) {
+                            error: function(xhr) {
                                 alert('An error occurred.');
                             }
                         });
@@ -254,7 +257,7 @@
             });
 
             function reindexRows() {
-                $('#departmentsTable tbody tr').each(function (index) {
+                $('#departmentsTable tbody tr').each(function(index) {
                     $(this).find('td:first').text(index + 1);
                 });
             }
