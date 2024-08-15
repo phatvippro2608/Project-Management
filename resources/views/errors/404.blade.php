735904ae46c88897ab6f21cb61fe0a9310e8b2ec
sxnd <sxuandi57@gmail.com>
2024-08-15 16:29:56 +0700

    .

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,22 +1,5 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>404</title>
-</head>
-<style>
-    *{
-        padding: 0;
-        margin: 0;
-        box-sizing: border-box;
-    }
-</style>
-<body>
-    <div style="height: 100vh; padding: 10px">
-        <img style="width: 100%; height: 100%" src="{{asset('assets/img/404.svg')}}" alt="">
-    </div>
-</body>
-</html>
+@extends('errors.master')
+
+@section('image')
+    <img style="width: 100%; height: 100%" src="{{asset('assets/img/404.svg')}}" alt="">
+@endsection
---
+++ resources/views/errors/master.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @yield('title')
+</head>
+<style>
+    *{
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+</style>
+<body>
+    <div style="height: 100vh; padding: 10px">
+        @yield('image')
+    </div>
+</body>
+</html>
