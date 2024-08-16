6fd4b58a2d1b0d1bc39a52eb9378521419116205
ttnhan2121 <ttnhan1212@hotmail.com>
2024-08-16 09:05:09 +0700

    complete

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\DepartmentModel;
+use App\Models\EmployeeModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Border;
 
 class DepartmentController extends Controller
 {
@@ -86,9 +90,96 @@ class DepartmentController extends Controller
 //        dd($data);
         return view('auth.departments.department',[
             'department_name' => $department_name,
+            'department_id' => $department_id,
             'data' => $data,
         ]);
     }
 
+    public function addEmployee(Request $request, $department_id){
+        $employees = explode(',', $request->employees);
+        $add = DB::table('employees')->whereIn('employee_id', $employees)->update(['department_id' => $department_id]);
+        if($add){
+            return response()->json(['message' => 'Action successful', 'status' => 200]);
+        }else{
+            return response()->json(['message' => 'Action failed', 'status' => 400]);
+        }
+    }
+
+    public function deleteEmployee(Request $request,$department_id, $employee_id){
+        $delete = DB::table('employees')
+            ->where('department_id', $department_id)
+            ->where('employee_id', $employee_id)
+            ->update(['department_id' => null]);
+
+        if($delete){
+            return response()->json(['message' => 'Action successful', 'status' => 200]);
+        }else{
+            return response()->json(['message' => 'Action failed', 'status' => 400]);
+        }
+    }
+
+    static public function getEmployeeNotInDepartment(){
+        $data = DB::table('employees')->where('department_id', null)->orderBy('employee_code')->get();
+        return $data;
+    }
+
+    public function export(Request $request, $department_id)
+    {
+        $inputFileName = public_path('excel-example/employee_department.xlsx');
+        $department_name = DB::table('departments')->where('department_id', $department_id)->value('department_name');
+        $inputFileType = IOFactory::identify($inputFileName);
+
+        $objReader = IOFactory::createReader($inputFileType);
+
+        $excel = $objReader->load($inputFileName);
+
+        $excel->setActiveSheetIndex(0);
+        $excel->getDefaultStyle()->getFont()->setName('Times New Roman');
+
+        $stt = 1;
+        $cell = $excel->getActiveSheet();
+
+        $data = DB::table('employees')
+            ->join('contacts', 'employees.contact_id', '=', 'contacts.contact_id')
+            ->join('accounts', 'employees.employee_id', '=', 'accounts.employee_id')
+            ->join('departments', 'employees.department_id', '=', 'departments.department_id')
+            ->where('employees.department_id', $department_id)->orderBy('employee_code')->get();
+        $num_row = 2;
+        foreach ($data as $row) {
+            $cell->setCellValue('A' . $num_row, $stt++);
+            $cell->setCellValue('B' . $num_row, $row->employee_code);
+            $cell->setCellValue('C' . $num_row, $row->first_name);
+            $cell->setCellValue('D' . $num_row, $row->last_name);
+            $cell->setCellValue('E' . $num_row, $row->en_name);
+            $cell->setCellValue('F' . $num_row, $row->phone_number);
+            $cell->setCellValue('G' . $num_row, $row->email ?? '');
+            $cell->setCellValue('H' . $num_row, $row->gender == 0 ? 'Male' : 'Female');
+            $cell->setCellValue('I' . $num_row, $row->marital_status);
+            $cell->setCellValue('J' . $num_row, $row->date_of_birth);
+            $cell->setCellValue('K' . $num_row, $row->national);
+            $cell->setCellValue('L' . $num_row, $row->military_service);
+            $cell->setCellValue('M' . $num_row, $row->cic_number);
+            $cell->setCellValue('N' . $num_row, $row->cic_issue_date);
+            $cell->setCellValue('O' . $num_row, $row->cic_expiry_date);
+            $cell->setCellValue('P' . $num_row, $row->cic_place_issue);
+            $cell->setCellValue('Q' . $num_row, $row->current_residence);
+            $cell->setCellValue('R' . $num_row, $row->permanent_address);
+            $cell->setCellValue('S' . $num_row, $row->department_name);
+            $borderStyle = $cell->getStyle('A'.$num_row.':S' . $num_row)->getBorders();
+            $borderStyle->getAllBorders()->setBorderStyle(Border::BORDER_THIN);
+            $cell->getStyle('A'.$num_row.':S' . $num_row)->getAlignment()->setHorizontal(Alignment::HORIZONTAL_LEFT);
+            $num_row++;
+        }
+        foreach (range('A', 'S') as $columnID) {
+            $excel->getActiveSheet()->getColumnDimension($columnID)->setAutoSize(true);
+        }
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        $filename = "Employees-List-Of-Department-". $department_name  . '.xlsx';
+        header('Content-Disposition: attachment;filename="' . $filename . '"');
+        header('Cache-Control: max-age=0');
+
+        $writer = IOFactory::createWriter($excel, 'Xlsx');
+        $writer->save('php://output');
+    }
 
 }
--- resources/views/auth/departments/department.blade.php
+++ resources/views/auth/departments/department.blade.php
@@ -18,15 +18,8 @@
                     {{ __('messages.add') }}
                 </div>
             </div>
-            <div class="btn btn-primary mx-2">
-                <a href="{{action('App\Http\Controllers\EmployeesController@importView')}}"
-                   class="d-flex align-items-center at2 text-white">
-                    <i class="bi bi-file-earmark-arrow-up pe-2"></i>
-                    {{ __('messages.import') }}
-                </a>
-            </div>
             <div class="btn btn-primary mx-2 btn-export">
-                <a href="{{action('App\Http\Controllers\EmployeesController@export')}}"
+                <a href="{{action('App\Http\Controllers\DepartmentController@export',['id' => $department_id])}}"
                    class="d-flex align-items-center text-white">
                     <i class="bi bi-file-earmark-arrow-down pe-2"></i>
                     {{ __('messages.export') }}
@@ -73,18 +66,14 @@
                                 <td>{{$item->en_name}}</td>
                                 <td class="text-center">{{ $item->gender == 0 ? __('messages.male') : __('messages.female') }}</td>
                                 <td class="text-center">
-                                        <?php
-                                        $id = $item->employee_id;
-                                        $item->email = \Illuminate\Support\Facades\DB::table('accounts')->where('employee_id', $id)->value('email');
-                                        ?>
-                                    <a href=""
+                                    <a href="{{action('App\Http\Controllers\EmployeesController@updateView',$item->employee_id)}}"
                                        class="btn p-0 btn-primary border-0 bg-transparent text-primary shadow-none at3">
                                         <i class="bi bi-pencil-square"></i>
                                     </a>
                                     |
                                     <button
                                         class="btn p-0 btn-primary border-0 bg-transparent text-danger shadow-none at4"
-                                        data="{{$id}}">
+                                        data="{{$item->employee_id}}">
                                         <i class="bi bi-trash3"></i>
                                     </button>
                                 </td>
@@ -92,6 +81,31 @@
                         @endforeach
                     </tbody>
                 </table>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal modal-add" tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Add Employee to Department</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <label for="select-employee">Choose Employee</label>
+                    <select class="form-select form-select-lg employee-list" size="20" multiple id="select-employee">
+                        @php
+                            $data = \App\Http\Controllers\DepartmentController::getEmployeeNotInDepartment();
+                        @endphp
+                        @foreach($data as $item)
+                            <option value="{{$item->employee_id}}">{{$item->employee_code}} - {{$item->last_name . ' ' . $item->first_name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary btn-add">Add</button>
+                </div>
             </div>
         </div>
     </div>
@@ -109,5 +123,70 @@
             },
             responsive: true
         });
+        let _add = '{{action('App\Http\Controllers\DepartmentController@addEmployee',['id' => $department_id])}}';
+        let _delete = '{{action('App\Http\Controllers\DepartmentController@deleteEmployee', ['id' => $department_id, 'employee_id' => ':employee_id'])}}';
+
+        $('.at1').click(function () {
+            $('.modal-add').modal('show');
+
+            $('.btn-add').click(function () {
+                let employees = $('.employee-list').val();
+                if(employees.length === 0){
+                    toastr.error('Please choose Employee to add to Department!', 'Error');
+                    return;
+                }
+                let formData = new FormData();
+                formData.append('employees', employees);
+                $.ajax({
+                    url: _add,
+                    type: 'POST',
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
+                    contentType: false,
+                    processData: false,
+                    data: formData,
+                    success: function (response) {
+                        if (response.status === 200) {
+                            toastr.success(response.message, response.message);
+                            setTimeout(function () {
+                                window.location.reload();
+                            }, 500);
+                        } else {
+                            toastr.error(response.message, response.message);
+                        }
+                    }
+                });
+            })
+        })
+
+        $('.at4').click(function () {
+            let employee_id = $(this).attr('data');
+            let url = _delete.replace(':employee_id', employee_id)
+            if(confirm('Are you sure you want to remove this employee from the department?')){
+                $.ajax({
+                    url: url,
+                    type: 'POST',
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
+                    contentType: false,
+                    processData: false,
+                    success: function (response) {
+                        console.log(typeof response.status)
+                        if (response.status === 200) {
+                            toastr.success(response.message, response.message);
+                            setTimeout(function () {
+                                window.location.reload();
+                            }, 500);
+                        } else {
+                            toastr.error(response.message, response.messages);
+                        }
+                    }
+                });
+            }
+        })
+
+
     </script>
 @endsection
